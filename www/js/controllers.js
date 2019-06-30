e025d31bf88902156ba39ed927a65ef9d27a2d70
akmal <udinakmal27@gmail.com>
2019-06-30 10:59:28 +0700

    initial commit

---
+++ www/js/controllers.js
@@ -0,0 +1,168 @@
+angular.module('app.controllers', [])
+  
+.controller('bLONTANGCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('pesananCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('keluarCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('menuCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('lOGINCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+	$scope.login = function()
+	{
+		location.href = '/aaa';
+	}
+
+
+}])
+   
+.controller('lokasiCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('editorBajuCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('editorTopiCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('editorMugCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('editorCaseHPCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('tersimpanCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('chatLangsungCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('chatCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('pILIHMEDIACtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('cHECKOUTCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('privacyPolicyCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('maafYaCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('detailPesananCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('mediaSosialCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('pendaftaranCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+ 
---
+++ www/js/routes.js
@@ -0,0 +1,175 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+
+      .state('menu.bLONTANG', {
+    url: '/home',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/bLONTANG.html',
+        controller: 'bLONTANGCtrl'
+      }
+    }
+  })
+
+  .state('menu.pesanan', {
+    url: '/pengaturan',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/pesanan.html',
+        controller: 'pesananCtrl'
+      }
+    }
+  })
+
+  .state('keluar', {
+    url: '/keluar',
+    templateUrl: 'templates/keluar.html',
+    controller: 'keluarCtrl'
+  })
+
+  .state('menu', {
+    url: '/side-menu21',
+    templateUrl: 'templates/menu.html',
+    controller: 'menuCtrl'
+  })
+
+  .state('lOGIN', {
+    url: '/login',
+    templateUrl: 'templates/lOGIN.html',
+    controller: 'lOGINCtrl'
+  })
+
+  .state('lokasi', {
+    url: '/lokasi',
+    templateUrl: 'templates/lokasi.html',
+    controller: 'lokasiCtrl'
+  })
+
+  .state('menu.editorBaju', {
+    url: '/editor',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/editorBaju.html',
+        controller: 'editorBajuCtrl'
+      }
+    }
+  })
+
+  .state('editorTopi', {
+    url: '/page17',
+    templateUrl: 'templates/editorTopi.html',
+    controller: 'editorTopiCtrl'
+  })
+
+  .state('editorMug', {
+    url: '/page18',
+    templateUrl: 'templates/editorMug.html',
+    controller: 'editorMugCtrl'
+  })
+
+  .state('menu.editorCaseHP', {
+    url: '/page7',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/editorCaseHP.html',
+        controller: 'editorCaseHPCtrl'
+      }
+    }
+  })
+
+  .state('tersimpan', {
+    url: '/page8',
+    templateUrl: 'templates/tersimpan.html',
+    controller: 'tersimpanCtrl'
+  })
+
+  .state('menu.chatLangsung', {
+    url: '/page9',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/chatLangsung.html',
+        controller: 'chatLangsungCtrl'
+      }
+    }
+  })
+
+  .state('menu.chat', {
+    url: '/page10',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/chat.html',
+        controller: 'chatCtrl'
+      }
+    }
+  })
+
+  .state('menu.pILIHMEDIA', {
+    url: '/page11',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/pILIHMEDIA.html',
+        controller: 'pILIHMEDIACtrl'
+      }
+    }
+  })
+
+  .state('cHECKOUT', {
+    url: '/page12',
+    templateUrl: 'templates/cHECKOUT.html',
+    controller: 'cHECKOUTCtrl'
+  })
+
+  .state('privacyPolicy', {
+    url: '/page13',
+    templateUrl: 'templates/privacyPolicy.html',
+    controller: 'privacyPolicyCtrl'
+  })
+
+  .state('menu.maafYa', {
+    url: '/page14',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/maafYa.html',
+        controller: 'maafYaCtrl'
+      }
+    }
+  })
+
+  .state('menu.detailPesanan', {
+    url: '/page15',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/detailPesanan.html',
+        controller: 'detailPesananCtrl'
+      }
+    }
+  })
+
+  .state('mediaSosial', {
+    url: '/page16',
+    templateUrl: 'templates/mediaSosial.html',
+    controller: 'mediaSosialCtrl'
+  })
+
+  .state('menu.pendaftaran', {
+    url: '/page19',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/pendaftaran.html',
+        controller: 'pendaftaranCtrl'
+      }
+    }
+  })
+
+$urlRouterProvider.otherwise('/login')
+
+
+});
